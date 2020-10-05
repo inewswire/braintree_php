92b563e6b9561cea49f1af56e3172aefdbc37068
Jonathan Pike <code@staite.net>
2020-10-05 02:58:31 +0000

    as object instead of array

--- lib/Braintree/Serializable.php
+++ lib/Braintree/Serializable.php
@@ -5,25 +5,24 @@ namespace Braintree;
 abstract class Serializable
 {
     protected $_attributes = array();
-    
-    public function serialize($as_array = false, $max_depth = PHP_INT_MAX)
+
+    public function serialize($as_object = false, $max_depth = PHP_INT_MAX)
     {
+        $object = new \stdClass();
         if ($max_depth <= 0) return null;
-        if (!isset($this->_attributes)) return array();
-        if (!is_array($this->_attributes)) return array();
-    
-        $attributes = array();
+        if (!isset($this->_attributes)) return $object;
+        if (!is_array($this->_attributes)) return $object;
 
         foreach ($this->_attributes as $k => $attribute)
         {
             if ($attribute instanceof Serializable)
-                 $attributes[$k] = $attribute->serialize(true, $max_depth-1);
-            else $attributes[$k] = $attribute;
+                 $object->$k = $attribute->serialize(true, $max_depth-1);
+            else $object->$k = $attribute;
         }
-        
-        if ($as_array)
-             return $attributes;
-        else return json_encode($attributes);
+
+        if ($as_object)
+             return $object;
+        else return json_encode($object);
     }
 }
 
